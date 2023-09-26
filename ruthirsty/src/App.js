6405fd47ffe0a2abd4b1784d33a1ecee116a6b50
alecchoi <alec.choi8@gmail.com>
2023-09-26 17:57:02 -0400

    First Draft UI
    yoo

--- ruthirsty/src/App.js
+++ ruthirsty/src/App.js
@@ -4,9 +4,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          RU Thirsty
-        </p>
+
+        <h1 id = "header">RU Thirsty</h1>
+        <h2>Where RU?</h2>
+
+        <a id = "busch" href="https://www.example.com" class="clickable-box">
+          <div>
+            Busch
+          </div>
+        </a>
+
+        <a id = "livi" href="https://www.example.com" class="clickable-box">
+          <div>
+            Livingston
+          </div>
+        </a>
+
+        <a id = "ca" href="https://www.example.com" class="clickable-box">
+          <div>
+            C/A
+          </div>
+        </a>
+
+        <a id = "cd" href="https://www.example.com" class="clickable-box">
+          <div>
+            C/D
+          </div>
+        </a>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
