a06b03063858bf494183c08d76be913a77d3bd1c
benjaminyu14 <benjamin14yu@gmail.com>
2023-10-07 11:35:23 -0400

    added react router dom

--- ruthirsty/src/App.js
+++ ruthirsty/src/App.js
@@ -7,7 +7,7 @@ import Home from './Home';
 function App() {
   return (
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/busch"element={<Busch />} />
       </Routes>
   );
---
+++ ruthirsty/src/components/busch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const busch = () => {
+    return (
+      <div>
+        <h1>busch</h1>
+      </div>
+    );
+  };
+    
+  export default busch;
