0c941744ee550a3fd9b9f19d5b6fa08cab44530f
nmazid121 <nmazid121@gmail.com>
2023-10-08 04:42:00 -0500

    Working test of placing markers
    - used the npm install command to install google maps api for react
    - created a container style for the maps within the header of the busch.js file
    - centered the map at the busch co-ordinates
    - created a variable that places a marker wherever the user clicks on the map
    - removed the api key for security purposes

--- ruthirsty/src/Busch.js
+++ ruthirsty/src/Busch.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './App.css';
 import logo from './images/Logo.PNG';
 import { useNavigate } from 'react-router-dom';
+import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
+const containerStyle = {
+    width: '90%',
+    height: '400px', // Adjust the height according to your design
+  };
 
 function Busch() {
+    
+    const center = { lat: 40.5221, lng: -74.4572 }; // Default coordinates for Busch Campus, where map starts
+    const [markers, setMarkers] = React.useState([]);
+    const onMapClick = (event) => {
+        setMarkers([...markers, { lat: event.latLng.lat(), lng: event.latLng.lng() }]);
+      }; // sets marker down when user clicks on map
+
     const navigate = useNavigate();
 
     const navigateHome = () => {
@@ -19,6 +31,18 @@ function Busch() {
             <button className="homeButton" onClick={navigateHome}>
             Home
             </button>
+            <LoadScript googleMapsApiKey="Paste Api Key Here!!!"> 
+                <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={15}
+                onClick={onMapClick}
+                >
+                {markers.map((marker, index) => (
+                    <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+                ))}
+                </GoogleMap>
+            </LoadScript>
             </header>
         </div>
     )
