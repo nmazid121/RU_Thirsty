91862777a8682de41b9f50e10522296de00db5c7
alecchoi <alec.choi8@gmail.com>
2023-10-18 15:09:38 -0400

    Busch.js changes
    Edited the water drop mostly. Still need to tune things out.

--- ruthirsty/src/Busch.js
+++ ruthirsty/src/Busch.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 import logo from './images/Logo.PNG';
 import waterIcon from './images/waterIcon.png';
 import { useNavigate } from 'react-router-dom';
 import { GoogleMap, Marker, LoadScript, InfoWindow } from '@react-google-maps/api';
-
-
 
 const containerStyle = {
     width: '90%',
@@ -18,6 +16,8 @@ function Busch() {
     const [selectedMarker, setSelectedMarker] = React.useState(null);
     const [locationName, setLocationName] = React.useState('');
     const [locationDescription, setLocationDescription] = React.useState('');
+    const [showConfirmation, setShowConfirmation] = React.useState(false);
+    const confirmationTimeout = useRef(null);
 
     const navigate = useNavigate();
 
@@ -29,47 +29,93 @@ function Busch() {
         const newMarker = { lat: event.latLng.lat(), lng: event.latLng.lng() };
         setMarkers([...markers, newMarker]);
         setSelectedMarker(newMarker);
+        setLocationName('');
+        setLocationDescription('');
     };
 
-    const handleCancel = () => {
-        setSelectedMarker(null);  // Hide the InfoWindow when cancel button is clicked
+    const handleOkClick = () => {
+        setShowConfirmation(true);
+        setSelectedMarker(null);
+        if (confirmationTimeout.current) {
+            clearTimeout(confirmationTimeout.current);
+        }
+        confirmationTimeout.current = setTimeout(() => {
+            setShowConfirmation(false);
+        }, 2000);
     };
+
+    const handleInfoWindowClose = () => {
+        setMarkers(prevMarkers => {
+            // Remove the last marker
+            const updatedMarkers = [...prevMarkers];
+            updatedMarkers.pop();
+            return updatedMarkers;
+        });
+        setSelectedMarker(null);
+    };
+
+    useEffect(() => {
+        return () => {
+            if (confirmationTimeout.current) {
+                clearTimeout(confirmationTimeout.current);
+            }
+        };
+    }, []);
 
     return (
         <div className="Busch">
             <header className="App-header">
                 <img id="logo" src={logo} alt="Logo" />
                 <h2>You are at Busch</h2>
+                {showConfirmation && 
+                    <div style={{ 
+                        position: 'fixed', 
+                        zIndex: 1000,
+                        top: '10%', 
+                        left: '50%', 
+                        transform: 'translate(-50%, -50%)', 
+                        color: 'blue', 
+                        fontSize: '24px', 
+                        fontWeight: 'bold', 
+                        backgroundColor: 'white', 
+                        padding: '10px', 
+                        borderRadius: '8px'
+                    }}>
+                        Water fountain has been marked!
+                    </div>
+                }
                 <button className="homeButton" onClick={navigateHome}>
                     Home
                 </button>
-                <LoadScript googleMapsApiKey="AIzaSyBEdVNIaYp-brYH2bDBj9b5H82a_ImiACc">
+                <LoadScript googleMapsApiKey="PASTE API KEY HERE">
                     <GoogleMap
                         mapContainerStyle={containerStyle}
                         center={center}
                         zoom={15}
                         onClick={onMapClick}
-                        mapTypeId = {'satellite'}
+                        mapTypeId={'satellite'}
                     >
                         {markers.map((marker, index) => (
                             <Marker 
                                 key={index} 
-                                position={{ lat: marker.lat, lng: marker.lng }} 
-                                onClick={() => setSelectedMarker(marker)}
-                                icon = {{url: waterIcon, scaledSize: new window.google.maps.Size(50, 50) }}
+                                position={marker} 
+                                onClick={() => {
+                                    setSelectedMarker(marker);
+                                }}
+                                icon={{url: waterIcon, scaledSize: new window.google.maps.Size(50, 50) }}
                             />
                         ))}
                         {selectedMarker && (
                             <InfoWindow
-                                position={{ lat: selectedMarker.lat, lng: selectedMarker.lng }}
-                                onCloseClick={() => setSelectedMarker(null)}
+                                position={selectedMarker}
+                                onCloseClick={handleInfoWindowClose}
                             >
-                                <div>
+                                <div style={{ fontFamily: 'Times New Roman', width: '200px' }}>
                                     <input 
                                         type="text" 
                                         value={locationName}
                                         onChange={(e) => setLocationName(e.target.value)}
-                                        placeholder="Insert Location Here"
+                                        placeholder="What Building?"
                                     />
                                     <br />
                                     <input 
@@ -79,7 +125,7 @@ function Busch() {
                                         placeholder="Description of Location"
                                     />
                                     <br />
-                                    <button onClick={handleCancel}>X</button>
+                                    <button onClick={handleOkClick}>OK</button>
                                 </div>
                             </InfoWindow>
                         )}
