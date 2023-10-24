85a27928ecb1a1a2d3bd2b7c112e8cd067c5ce43
alecchoi <alec.choi8@gmail.com>
2023-10-24 13:01:54 -0400

    home page changes

--- ruthirsty/src/Home.js
+++ ruthirsty/src/Home.js
@@ -23,25 +23,28 @@ function Home() {
     }
 
     return (
-    <div className="App">
-        <header className="App-header">
-          <img id="logo" src={logo} alt="Logo" />
-          <h2>Where RU?</h2>
-          <button className="buschButton" onClick={navigateBusch}>
-            Busch
-            </button>
-          <button className="livingstonButton" onClick={navigateLivingston}>
-            Livingston
-            </button>
-          <button className="collegeaveButton" onClick={navigateCollegeAve}>
-            CollegeAve
-          </button>
-          <button className="cookdougButton" onClick={navigateCookDoug}>
-            CookDoug
-          </button>
+      <div className="App">
+          <header className="App-header">
+            <img id="logo" src={logo} alt="Logo" />
+            <h2>Where RU?</h2>
+            <div className="button-grid">
+              <button className="buschButton" onClick={navigateBusch}>
+                  Busch
+              </button>
+              <button className="livingstonButton" onClick={navigateLivingston}>
+                  Livingston
+              </button>
+              <button className="collegeaveButton" onClick={navigateCollegeAve}>
+                  CollegeAve
+              </button>
+              <button className="cookdougButton" onClick={navigateCookDoug}>
+                  CookDoug
+              </button>
+            </div>
           </header>
-        </div>
-    );
+      </div>
+  );
+  
 }
 
 export default Home;
