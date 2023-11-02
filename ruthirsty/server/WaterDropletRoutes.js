850e02455df0088dd8546b160ce41f3813a54420
nmazid121 <nmazid121@gmail.com>
2023-11-02 10:33:44 -0500

    new backend

--- ruthirsty/server/WaterDropletRoutes.js
+++ ruthirsty/server/WaterDropletRoutes.js
@@ -6,13 +6,21 @@ const WaterDroplet = require('./WaterDroplet');
 router.post('/droplets', async (req, res) => {
   try {
     const { lat, lng, name, description } = req.body;
-    const newDroplet = new WaterDroplet({ lat, lng, name, description });
+    const newDroplet = new WaterDroplet({
+      location: {
+        type: 'Point',
+        coordinates: [lng, lat] // Note that GeoJSON format is [longitude, latitude]
+      },
+      name,
+      description
+    });
     await newDroplet.save();
     res.status(201).json(newDroplet);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
 
 // Get all water droplets
 router.get('/droplets', async (req, res) => {
@@ -25,13 +33,31 @@ router.get('/droplets', async (req, res) => {
 });
 
 // Update a specific water droplet
+// Update a specific water droplet
 router.patch('/droplets/:id', async (req, res) => {
-  // Update logic here
+  try {
+    const droplet = await WaterDroplet.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!droplet) {
+      return res.status(404).json({ message: "Droplet not found" });
+    }
+    res.json(droplet);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Delete a specific water droplet
 router.delete('/droplets/:id', async (req, res) => {
-  // Deletion logic here
+  try {
+    const droplet = await WaterDroplet.findByIdAndDelete(req.params.id);
+    if (!droplet) {
+      return res.status(404).json({ message: "Droplet not found" });
+    }
+    res.json({ message: "Droplet deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
 
 module.exports = router;
--- ruthirsty/server/server.js
+++ ruthirsty/server/server.js
@@ -1,26 +1,57 @@
 const express = require('express');
-const mongoose = require('./db');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const waterDropletRoutes = require('./WaterDropletRoutes'); // Import your routes
+const mongoose = require('mongoose');
 
 const app = express();
+const PORT = 5000;
 
-// Middleware
-app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
-// MongoDB connection setup (in db.js)
-require('./db'); // Assuming your MongoDB connection setup code is in db.js
-
-// Routes
-app.use('/api/waterDroplets', waterDropletRoutes); // Use your water droplet routes at /api/waterDroplets
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Connect to MongoDB Atlas
+const mongoUri = 'mongodb+srv://newUserYuh:FartPoop@cluster0.fv35vec.mongodb.net/?retryWrites=true&w=majority'; // Replace with your connection string from Atlas
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// Start the server
-const PORT = process.env.PORT || 5000;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define Mongoose Schema and Model for Markers
+const markerSchema = new mongoose.Schema({
+  lat: Number,
+  lng: Number,
+  // Add other fields if necessary
+});
+const Marker = mongoose.model('Marker', markerSchema);
+
+// Express routes
+app.post('/api/markers', (req, res) => {
+  const marker = new Marker(req.body);
+  marker.save((err) => {
+    if (err) {
+      res.status(500).send('Error saving marker');
+      return;
+    }
+    res.status(201).send('Marker saved');
+  });
+});
+
+app.get('/api/markers', (req, res) => {
+  Marker.find({}, (err, markers) => {
+    if (err) {
+      res.status(500).send('Error fetching markers');
+      return;
+    }
+    res.json(markers);
+  });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
