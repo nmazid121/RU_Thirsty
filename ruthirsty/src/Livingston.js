c9d7f31529c5480b503ddcfe5899054d1d16c859
nmazid121 <nmazid121@gmail.com>
2023-11-07 20:08:35 -0500

    RUThirsty v1
    - fully working front end + backend, frontend hosted by Firebase, backend hosted by Render

--- ruthirsty/src/Livingston.js
+++ ruthirsty/src/Livingston.js
@@ -76,7 +76,7 @@ function Livingston() {
             name: locationName,
         };
         
-        axios.post('http://localhost:5000/api/markers', newMarkerData)
+        axios.post('https://ruthirsty.onrender.com/api/markers', newMarkerData)
             .then(response => {
                 console.log(response.data);
                 // You can set a state or provide feedback to the user if needed
@@ -123,7 +123,7 @@ function Livingston() {
     };
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/markers')
+        axios.get('https://ruthirsty.onrender.com/api/markers')
             .then(response => {
                 setMarkers(response.data);  // Update the markers state with the fetched data
             })
