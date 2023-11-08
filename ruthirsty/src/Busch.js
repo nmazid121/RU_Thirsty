d9b30da304fb96d0c4fd029056b14f3d89704226
nmazid121 <nmazid121@gmail.com>
2023-11-07 19:24:55 -0500

    render backend edits

--- ruthirsty/src/Busch.js
+++ ruthirsty/src/Busch.js
@@ -80,7 +80,7 @@ function Busch() {
             name: locationName,
         };
         
-        axios.post('/api/markers', newMarkerData)
+        axios.post('${process.env.REACT_APP_API_URL}/api/markers', newMarkerData)
             .then(response => {
                 console.log(response.data);
                 // You can set a state or provide feedback to the user if needed
@@ -127,7 +127,7 @@ function Busch() {
     };
 
     useEffect(() => {
-        axios.get('/api/markers')
+        axios.get('http://localhost:5000/api/markers')
             .then(response => {
                 setMarkers(response.data);  // Update the markers state with the fetched data
             })
